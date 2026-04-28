628e87bd32b2e95be161c3dc14084b74bf9ba15d
Ömerfaruk <github_yukselomerfaruk292@gmail.com>
2026-04-28 22:15:58 +0300

    Logo ismi degistirildi ve zorlandi

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ export const metadata: Metadata = {
   title: "SİNEPRO | Film ve Dizi Portalı",
   description: "En güncel içerikler ve favori listeniz.",
   icons: {
-    icon: "/icon.png",
+    icon: "/logo-sinepro.png",
   },
 };
 
@@ -16,6 +16,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="tr">
+      <head>
+      {/* Bu satır tarayıcıyı logoyu almaya zorlar */}
+      <link rel="icon" href="/logo-sinepro.png" sizes="any" />
+    </head>
       <body>{children}</body>
     </html>
   );
