7a0058357c6ac27cd2554821334fa1da06e75e93
Ömerfaruk <github_yukselomerfaruk292@gmail.com>
2026-04-29 14:00:50 +0300

    son güncelleme yapıldı

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,13 +3,27 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "SİNEPRO | Film ve Dizi Portalı",
-  description: "En güncel içerikler ve favori listeniz.",
+  description: "En güncel içerikler, reklamsız HD film ve dizi keyfi SinePro'da!", // Açıklamayı biraz daha ilgi çekici yaptık
   icons: {
     icon: "/icon.png",
     apple: "/icon.png",
   },
   verification: {
     google: "lmDwjSLizRKR5WITo7zpUv0pwyVdP57ntVRqlDOFbqo",
+  },
+  // WHATSAPP VE SOSYAL MEDYA ÖNİZLEMESİ İÇİN:
+  openGraph: {
+    title: "SİNEPRO | Film ve Dizi Portalı",
+    description: "Reklamsız ve hızlı film izlemenin adresi.",
+    type: "website",
+    url: "https://farukomer46.vercel.app/", // Kendi site adresin
+    images: [
+      {
+        url: "/icon.png", // Link atınca görünecek resim (logosunu koyduk)
+        width: 800,
+        height: 600,
+      },
+    ],
   },
 };
 
